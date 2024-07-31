4174e8a40e585b84bb2681c6a5edae0c6acbd7e3
xuechenliu <xuecliu@nii.ac.jp>
2024-07-31 22:44:42 +0900

    use parselmouth for pitch

--- scripts/asv_cm_explainability/physics_labels/write_utt2pitch.py
+++ scripts/asv_cm_explainability/physics_labels/write_utt2pitch.py
@@ -2,18 +2,32 @@
 Average the pitch features and store them into the numpy tarball
 """
 
-import kaldi_io
-import numpy as np
+import glob
 import os
 import sys
 
+import parselmouth
+
+
+def extract_pitch(audio_path):
+    # Load the audio file
+    snd = parselmouth.Sound(audio_path)
+    # Extract pitch using Parselmouth
+    pitch = snd.to_pitch()
+    # Get pitch values in Hz
+    pitch_values = pitch.selected_array["frequency"]
+    # Remove unvoiced pitch values (represented by 0 Hz)
+    pitch_values = pitch_values[pitch_values > 0]
+
+    return pitch_values.mean()
+
 
 if __name__ == "__main__":
-    scp_file = sys.argv[1]
-    tar_dir = sys.argv[2]
-    os.makedirs(tar_dir, exist_ok=True)
-    with open(tar_dir + "/utt2pitch", "w") as t:
-        for key, mat in kaldi_io.read_mat_scp(scp_file):
-            vec = np.mean(mat, axis=0)
-            pitch_energy = vec[0]
-            t.write("{} {}\n".format(key, pitch_energy))
+    data_dir = sys.argv[1]
+
+    flac_files = glob.glob(data_dir + "/trimmed/*.wav")
+    with open(data_dir + "/utt2pitch", "w") as w:
+        for f in flac_files:
+            utt = os.path.basename(f).split(".")[0]
+            dur = extract_pitch(f)
+            w.write("{} {}\n".format(utt, dur))
