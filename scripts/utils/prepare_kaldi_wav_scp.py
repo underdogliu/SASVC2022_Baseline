42c3073973ad6bdad3a497e1618c201521fcf987
xuechenliu <xuecliu@nii.ac.jp>
2024-07-31 21:47:01 +0900

    update scp writing for pitch estimation

---
+++ scripts/utils/prepare_kaldi_wav_scp.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+import glob
+
+
+if __name__ == "__main__":
+    data_dir = sys.argv[1]
+    wav_files = data_dir + "/trimmed/*.wav"
+
+    with open(data_dir + "/wav.scp", "w") as w:
+        for wav_path in wav_files:
+            utt = os.path.basename(wav_path).split(".")[0]
+            w.write("{} {}\n".format(utt, wav_path))
